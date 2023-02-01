af493d17d353c0cf5b3de1372da0085bf0ef8921
matthew-beep <herram@uw.edu>
2023-02-01 15:04:57 -0800

    Comments for filter feature

--- src/Components/GetInvolved/mission.js
+++ src/Components/GetInvolved/mission.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
-// Adding a comment
+
 export default function Mission(props) {
+
+    // adding filter feature
+    // add tag/category to organization in JSON file
+    // create drop down menu
+    // create filter options
+    // filter function
+    
 };
