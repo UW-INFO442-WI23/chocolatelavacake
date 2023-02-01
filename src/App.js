1f4d48a248aa69ce61e2df0db3939398ad97492c
jckim1116 <jck1116@uw.edu>
2023-02-01 10:59:08 -0800

    create components

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+
+export default function App(props) {
+    return (
+      <div>
+        <Router>
+          <Routes>
+  
+          </Routes>
+        </Router> 
+      </div>
+    );
+  }
+  
