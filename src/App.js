fe85176eb9dd9fc13fd5d075fbb7c259b157e5d1
matthew-beep <herram@uw.edu>
2023-02-02 11:54:04 -0800

    Started working on adding a logo and navbar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-d
 import Home from './Components/Home/home.js';
 import About from './Components/AboutUs/about.js';
 import Mission from './Components/GetInvolved/mission.js'
+import Nav from './Components/Nav/nav.js';
 
 export default function App(props) {
     return (
-      <div>
+      <div className="App">
+        <header>
+          <h1>logo</h1>
+          <Nav />
+        </header>
         <Router>
           <Routes>
             <Route path="/Home" element={<Home/>} />
