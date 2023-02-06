728aec70fcc7e5539808d6b845e3c956d9806a5d
matthew-beep <herram@uw.edu>
2023-02-06 10:32:26 -0800

    Updated nav bar

--- src/Components/Nav/nav.js
+++ src/Components/Nav/nav.js
@@ -5,9 +5,13 @@ export default function Nav(props) {
     return(
         <nav>
             <ul>
-                <li><a className="navigator" href="/Home">Home</a></li>
-                <li><a className="navigator" href="/About">About</a></li>
+                <li><a className="navigator" href="/Home">Introduction</a></li>
+                <li>|</li>
+                <li><a className="navigator" href="/About">Interactive Map</a></li>
+                <li>|</li>
                 <li><a className="navigator" href="/Mission">Get Involved</a></li>
+                <li>|</li>
+                <li><a className="navigator" href="/Mission">About Us</a></li>
             </ul>
         </nav>
     )
