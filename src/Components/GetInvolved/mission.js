7135792b7f92b6170f1e14356768e5632bb8f17f
matthew-beep <herram@uw.edu>
2023-02-27 14:47:40 -0800

    Finished filter

--- src/Components/GetInvolved/mission.js
+++ src/Components/GetInvolved/mission.js
@@ -19,37 +19,48 @@ export default function Mission(props) {
     let shownData = [...orgArray];
 
     const [appliedLocation, applyLocation] = useState([]);
-    const [appliedTags, applyTags] = useState(false);
+    const [appliedTags, applyTags] = useState([]);
 
     
 
     let rows = results(orgArray);
 
-    const checkBox = (event) => {
+    const checkBoxLocation = (event) => {
         if (event.target.checked) {
             applyLocation([...appliedLocation, event.target.value]);
           } else {
             applyLocation(
               appliedLocation.filter((filterTag) => filterTag !== event.target.value)
             )
-            
           }
     }
 
-    if (appliedLocation.length > 0) {
+    const checkBoxTags = (event) => {
+        if (event.target.checked) {
+            applyTags([...appliedTags, event.target.value]);
+          } else {
+            applyTags(
+              appliedTags.filter((filterTag) => filterTag !== event.target.value)
+            )
+          }
+    }
+    
+    if (appliedLocation.length > 0 || appliedTags > 0) {
+
         let filterData = [...orgArray];
-        //for (int i = 0; i < )
         filterData = filterData.filter((org) => {
-            // org.location == appliedLocation[0]
-            // appliedLocation.every(i => org.location.includes(i))
             return (appliedLocation.every(i => org.location.includes(i)));
+        });
+
+        filterData = filterData.filter((org) => {
+            return (appliedTags.every(i => org.tags.includes(i)));
         });
         shownData = filterData;
         rows = results(filterData);
     } else {
         shownData = DATA;
     }
-
+   
     /* When user searches in search bar */
     const handleChange = (event) => {
         const typedContent = event.target.value;
@@ -65,131 +76,11 @@ export default function Mission(props) {
         rows = results(data);
     }
 
-    // Return orgs to show
-
-
-    // filter functionality
-
-
-
     const handleClick = (event) => { // make box show up when clicked
         console.log("clicked");
         var element = document.getElementById('overlay');
         element.classList.toggle("show");
     }
-    
-    const HandleApply = (event) => {
-        
-        /*
-        let data = [...orgArray]; 
-        let filter = [];
-        let checkedElements = document.getElementsByClassName("filter-selection");
-        for (let i = 0; i < checkedElements.length; i++) { // add selected elements to array
-
-            if (checkedElements[i].checked) {
-                if (checkedElements[i].id == "US" || checkedElements[i].id == "International") {
-                    locationChecked = true;
-                    
-                    console.log(locationChecked);
-                    
-                }
-                filter.push(checkedElements[i].id);
-                
-            } 
-        }
-        
-        console.log(appliedLocation);
-        if (filter.length > 0) {
-            
-            for (let i = 0; i < filter.length; i++) {
-                console.log("checking " + filter[i]);
-                data = data.filter((org) => {
-                    return (org.location == filter[i]);
-                });
-            }
-            console.log(data);
-            changeCards(data);
-            
-        }
-        
-        console.log(rows);
-        */
- 
-
-        
-        var usVar = document.getElementById("US");
-        var internationalVar = document.getElementById("International");
-        var politicalVar = document.getElementById("Political");
-        var violenceVar = document.getElementById("Violence");
-        var economicVar = document.getElementById("Economic");
-        var healthVar = document.getElementById("Health");
-
-        let setLocationFilter = [];
-        let setTagFilter = [];
-
-        if (usVar.checked || internationalVar.checked) {
-            
-            
-            if (usVar.checked) {
-                setLocationFilter.push(usVar.id);
-                console.log("Looking for US organizations");
-                
-            } 
-            
-            if (internationalVar.checked) {
-                setLocationFilter.push(internationalVar.id);
-                console.log("Looking for International organizations");
-                
-            }
-            
-        }
-        applyLocation(setLocationFilter);
-
-        
-        if (politicalVar.checked || violenceVar.checked || economicVar.checked || healthVar.checked) {
-            applyTags(true);
-            if (politicalVar.checked) {
-                setTagFilter.push(politicalVar.id);
-                console.log("Looking for political organizations");
-            } 
-            
-            if (violenceVar.checked) {
-                setTagFilter.push(violenceVar.id);
-                console.log("Looking for violence organizations");
-            }
-
-            if (economicVar.checked) {
-                setTagFilter.push(economicVar.id);
-                console.log("Looking for economic organizations");
-            }
-
-            if (healthVar.checked) {
-                setTagFilter.push(healthVar.id);
-                console.log("Looking for health organizations");
-            }
-        }
-        applyTags(setTagFilter);
-    }
-
-
-    //changeCards(filteredData);
-
-    /*
-    if (appliedLocation.length > 0) {
-        let data =[...orgArray];
-        for (let i = 0; i < appliedLocation.length; i++) {
-            console.log("checking " + appliedLocation[i]);
-            data = data.filter((org) => {
-                return (org.location == appliedLocation[i]);
-            });
-        }
-        changeCards(data);
-    }
-    */
-
-
-
-
     
     return(
         <div className='container-mission'>
@@ -211,20 +102,20 @@ export default function Mission(props) {
                         <h4>Location</h4>
                     </div>
 
-                    <input type='checkbox' id="US" className='filter-selection' onChange={checkBox} value = "US"/>
+                    <input type='checkbox' id="US" className='filter-selection' onChange={checkBoxLocation} value = "US"/>
                     <label for="location1"> US </label><br/>
-                    <input type='checkbox' id="International" className='filter-selection' onChange={checkBox} value = "International"/>
+                    <input type='checkbox' id="International" className='filter-selection' onChange={checkBoxLocation} value = "International"/>
                     <label for="location2"> International </label><br/>
 
                     <h4>Organization Type</h4>
 
-                    <input type='checkbox' id="Political" className='filter-selection'/>
+                    <input type='checkbox' id="Political" className='filter-selection' onChange={checkBoxTags} value="Political"/>
                     <label for="tag1"> Political </label><br/>
-                    <input type='checkbox' id="Violence" className='filter-selection'/>
+                    <input type='checkbox' id="Violence" className='filter-selection' onChange={checkBoxTags} value="Violence"/>
                     <label for="tag2"> Violence </label><br/>
-                    <input type='checkbox' id="Economic" className='filter-selection'/>
+                    <input type='checkbox' id="Economic" className='filter-selection' onChange={checkBoxTags} value="Economic"/>
                     <label for="tag3"> Economic </label><br/>
-                    <input type='checkbox' id="Health" className='filter-selection'/>
+                    <input type='checkbox' id="Health" className='filter-selection' onChange={checkBoxTags} value="Health"/>
                     <label for="tag4"> Health </label><br/>
                     
                 </div>
