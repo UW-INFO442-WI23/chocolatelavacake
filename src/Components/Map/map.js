39dd20dfb670124fc6c0de32e8d191f56c0c5120
jckim1116 <jck1116@uw.edu>
2023-02-27 15:09:06 -0800

    add tableau map

--- src/Components/Map/map.js
+++ src/Components/Map/map.js
@@ -9,9 +9,8 @@ export default function map(props) {
             <h2>
                 Interactive Map
             </h2>
-            <div className='map'>
-                
-            </div>
+            
+            <iframe id='tableau' src='https://public.tableau.com/views/INFO-442-project-Wage-stats/Dashboard1?:language=en-US&:display_count=n&:origin=v:showVizHome=no&:embed=true'/>
         </div>
     );
     
