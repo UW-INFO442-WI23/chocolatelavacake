38fd3fc4c854e863a219d24070a542ea0446b006
jckim1116 <jck1116@uw.edu>
2023-03-01 15:06:19 -0800

    add content to introduction section and fixed media query for tableau map

--- src/Components/Home/home.js
+++ src/Components/Home/home.js
@@ -4,26 +4,36 @@ export default function Home(props) {
         <div className='container-home'>
             <div className='paragraph-one'>
                 <p className='text'>
-                Lorem ipsum dolor sit amet. Vel enim officia et 
-                amet quasi in voluptatem provident vel animi 
-                internos et voluptatem molestias non consequatur 
-                totam! Ut obcaecati enim cum veniam tempore hic 
-                quidem consectetur ut consectetur alias.
+                Welcome to our gender equality web application! 
+                Our goal is to provide users with an interactive map that showcases gender wage gap statistics across the United States, 
+                an industry based wage statistics graph,
+                as well as a comprehensive directory of gender equality organizations. 
+                We believe that it's crucial to raise awareness about gender inequality and 
+                to provide resources for people who want to get involved and make a difference. 
+                Our application is designed to be user-friendly and informative, and we hope it will
+                 inspire you to take action and join the movement toward gender equality.
+
                 </p>
                 <div className='img-box'>
-
+                    <iframe id='video-1' title='UN SDG video' src="https://www.youtube.com/embed/kirtWKKAPNs" 
+                    frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; 
+                    picture-in-picture; web-share" allowfullscreen ></iframe>
                 </div>
             </div>
             <div className='paragraph-two'>
                 <p className='text'>
-                Lorem ipsum dolor sit amet. Vel enim officia et 
-                amet quasi in voluptatem provident vel animi 
-                internos et voluptatem molestias non consequatur 
-                totam! Ut obcaecati enim cum veniam tempore hic 
-                quidem consectetur ut consectetur alias.
+                Gender equality is an essential issue that affects people of all genders, ages, and backgrounds. 
+                Despite significant progress in recent years, we still have a long way to go in achieving true gender 
+                equality. Our web application is designed to be a hub of information and resources for people who want 
+                to learn more about this critical issue and get involved in the fight for gender equality. With an 
+                interactive map that showcases gender wage gap statistics and a comprehensive directory of gender equality 
+                organizations, our application provides users with the tools and information they need to take action and 
+                make a difference.
                 </p>
                 <div className='img-box'>
-
+                    <iframe id='video-2' title='UN Gender Equality Vid' src="https://www.youtube.com/embed/ZC9jdhi6xgA" 
+                        frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; 
+                        picture-in-picture; web-share" allowfullscreen ></iframe>
                 </div>
             </div>
         </div>
