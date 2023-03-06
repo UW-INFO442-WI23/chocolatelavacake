059773b64f884cc248e928e67d7656808dfe50f7
jckim1116 <jck1116@uw.edu>
2023-03-06 15:06:39 -0800

    replace intro photo

--- src/Components/Home/home.js
+++ src/Components/Home/home.js
@@ -3,7 +3,7 @@ export default function Home(props) {
     return (
         <div className='container-home'>
             <div>
-            <img src="IntroPhoto.jpg" alt="Group of Women Smiling" className='header-photo'></img>
+            <img src="IntroPhoto.png" alt="Group of Women Smiling" className='header-photo'></img>
                 <h1>
                     Introduction
                 </h1>
